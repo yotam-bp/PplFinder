1187d9be8e81911b76a0909b1d10dfe5c17f1b7d
yotam-bp <yotyyybp@gmail.com>
2021-12-12 00:32:14 +0200

    Updates

---
+++ precache-manifest.5a2352e88b72d0888d56d1bb9451df11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ad72e01d747b9927b66e7b0ce0d5cd",
+    "url": "/PplFinder/index.html"
+  },
+  {
+    "revision": "981b66cba67680306184",
+    "url": "/PplFinder/static/js/2.ae88284e.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/PplFinder/static/js/2.ae88284e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7efdf525239462cec7",
+    "url": "/PplFinder/static/js/main.2194368e.chunk.js"
+  },
+  {
+    "revision": "32eb9f5c17181ba0ef19",
+    "url": "/PplFinder/static/js/runtime-main.b6c1fb24.js"
+  }
+]);
